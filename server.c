1bcb1eed1c047756bcd353bea5f33870e82f45ed
Reuben Hawkins <reubenhwk@gmail.com>
2010-12-01 21:20:58 -0800

    binding to all sockets

--- server.c
+++ server.c
@@ -15,7 +15,7 @@
 #include <sys/wait.h>
 #include <signal.h>
 
-#define PORT "3490"  // the port users will be connecting to
+#define PORT 3490  // the port users will be connecting to
 
 #define BACKLOG 10     // how many pending connections queue will hold
 
@@ -34,56 +34,39 @@ void *get_in_addr(struct sockaddr *sa)
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
+int
+all_sock (unsigned short port)
+{
+        struct sockaddr_in6 saddr;
+        int sock = socket (AF_INET6, SOCK_STREAM, 0);
+        int const one = 1;
+
+        if (sock < 0) {
+                exit (1);
+        }
+
+        memset (&saddr, 0, sizeof (saddr));
+        saddr.sin6_port = htons (port);
+
+        setsockopt (sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof (one));
+
+        bind (sock, (struct sockaddr *) &saddr, sizeof (saddr));
+        listen (sock, 15);
+
+        return sock;
+}
+
+
+
 int main(void)
 {
     int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
-    struct addrinfo hints, *servinfo, *p;
     struct sockaddr_storage their_addr; // connector's address information
     socklen_t sin_size;
     struct sigaction sa;
-    int yes=1;
     char s[INET6_ADDRSTRLEN];
-    int rv;
 
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE; // use my IP
-
-    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        return 1;
-    }
-
-    // loop through all the results and bind to the first we can
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                p->ai_protocol)) == -1) {
-            perror("server: socket");
-            continue;
-        }
-
-        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-                sizeof(int)) == -1) {
-            perror("setsockopt");
-            exit(1);
-        }
-
-        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-            close(sockfd);
-            perror("server: bind");
-            continue;
-        }
-
-        break;
-    }
-
-    if (p == NULL)  {
-        fprintf(stderr, "server: failed to bind\n");
-        return 2;
-    }
-
-    freeaddrinfo(servinfo); // all done with this structure
+	sockfd = all_sock(PORT);
 
     if (listen(sockfd, BACKLOG) == -1) {
         perror("listen");
